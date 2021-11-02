4d735c2be3d0509dad528bf95d3ee5b681d5f2bb
Arun Alexander A <90493136+aa-alexander@users.noreply.github.com>
2021-11-02 12:38:08 +0530

    Update fastqcRules.py

--- fastqcRules.py
+++ fastqcRules.py
@@ -3,8 +3,8 @@
 import glob
 import os
 
-inputPath = /path/to/rawReads    #path to your raw reads
-finalPath = /path/to/output   #path to your output directory
+inputPath = "/path/to/rawReads"    #path to your raw reads
+finalPath = "/path/to/output"   #path to your output directory
 nthreads = 4       #number of threads
 
 SRR, FR = glob_wildcards(inputPath + "/{srr}_{fr}.fastq") 
